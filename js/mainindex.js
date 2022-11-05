8616658d9b7f69bbc94c25269802a52e84565d7d
EzequielRomero7613 <romeroeze7613@gmail.com>
2022-11-04 21:18:13 -0300

    update mainjs

--- js/mainindex.js
+++ js/mainindex.js
@@ -2,14 +2,14 @@
 function facil(){
     onclick = function(){
         document.getElementById("facil");
-        this.location.href ="../html/juegofacil.html";
+        location.href = "../html/juegofacil.html";
     }
 }
 
 function media(){
     onclick = function(){
         document.getElementById("media");
-        this.location.href ="..html/juegodmedio.html";
+        this.location.href = "..html/juegodmedio.html";
     }
 }
 
@@ -17,6 +17,6 @@ function media(){
 function dificil(){
     onclick = function(){
         document.getElementById("dificil");
-        this.location.href ="../html/juegodificil.html";
+        this.location.href = "../html/juegodificil.html";
     }
 }
